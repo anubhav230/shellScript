43c3c76086965807bb265cd3cceacd6dcab5fb19
anubhav230 <anubhavsingh230@gmail.com>
2020-05-01 12:05:32 +0530

    Employee attendance check feature added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,9 @@
 echo "Welcome to the employee wage computation program"
+isPresent=1
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "Employee is present:"
+else
+	echo "employee is abscent:"
+fi
